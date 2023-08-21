f3d51b78f14ba6ee5a0563505b0ef9e648626392
Federico Matteoni <fedex15@live.it>
2023-08-21 15:35:35 +0200

    Changed app name and mainpage

--- shared/src/desktopMain/kotlin/main.desktop.kt
+++ shared/src/desktopMain/kotlin/main.desktop.kt
@@ -3,10 +3,10 @@ import androidx.compose.runtime.Composable
 
 actual fun getPlatformName(): String = "Desktop"
 
-@Composable fun MainView() = App()
+@Composable fun MainView() = MainPage()
 
 @Preview
 @Composable
 fun AppPreview() {
-    App()
+    MainPage()
 }
