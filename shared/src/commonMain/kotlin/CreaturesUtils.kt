934cd1343d463618c7f55d2a6b0868c973b18c29
Federico Matteoni <fedex15@live.it>
2023-08-22 08:07:24 +0200

    UI Fixes

--- shared/src/commonMain/kotlin/CreaturesUtils.kt
+++ shared/src/commonMain/kotlin/CreaturesUtils.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -55,8 +56,8 @@ enum class Type {
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun CreatureCard(creature: Creature) {
-    Card(modifier = Modifier.wrapContentHeight().fillMaxWidth()) {
-        Row(modifier = Modifier.wrapContentHeight().fillMaxWidth()) {
+    Card(modifier = Modifier.wrapContentHeight().fillMaxWidth().padding(8.dp)) {
+        Row(modifier = Modifier.wrapContentHeight().fillMaxWidth().padding(4.dp)) {
             Image(
                 painterResource(creature.image),
                 null,
@@ -64,10 +65,13 @@ fun CreatureCard(creature: Creature) {
             )
             Spacer(modifier = Modifier.width(8.dp))
             Column {
-                Text("${creature.number}", fontSize = 8.sp)
+                Text("${creature.number}", fontSize = 10.sp)
                 Text(creature.name, fontSize = 16.sp)
-                TypePill(creature.type1)
-                TypePill(creature.type2)
+                Row {
+                    TypePill(creature.type1)
+                    Spacer(modifier = Modifier.width(4.dp))
+                    TypePill(creature.type2)
+                }
             }
         }
     }
@@ -75,25 +79,25 @@ fun CreatureCard(creature: Creature) {
 
 fun getTypeColor(type: Type): Color {
     return when (type) {
-        Type.Grass -> Color("7AC74C".toLong(radix = 16))
-        Type.Water -> Color("6390F0".toLong(radix = 16))
-        Type.Fire -> Color("EE8130".toLong(radix = 16))
-        Type.Ground -> Color("E2BF65".toLong(radix = 16))
-        Type.Ghost -> Color("735797".toLong(radix = 16))
-        Type.Dragon -> Color("6F35FC".toLong(radix = 16))
-        Type.Ice -> Color("96D9D6".toLong(radix = 16))
-        Type.Rock -> Color("B6A136".toLong(radix = 16))
-        Type.Steel -> Color("B7B7CE".toLong(radix = 16))
-        Type.Fairy -> Color("D685AD".toLong(radix = 16))
-        Type.Electric -> Color("F7D02C".toLong(radix = 16))
-        Type.Dark -> Color("705746".toLong(radix = 16))
-        Type.Bug -> Color("A6B91A".toLong(radix = 16))
-        Type.Poison -> Color("A33EA1".toLong(radix = 16))
-        Type.Normal -> Color("A8A77A".toLong(radix = 16))
-        Type.Psychic -> Color("F95587".toLong(radix = 16))
-        Type.Fighting -> Color("C22E28".toLong(radix = 16))
-        Type.Flying -> Color("A98FF3".toLong(radix = 16))
-        Type.UNKNOWN -> Color("A8A77A".toLong(radix = 16))
+        Type.Grass -> Color("FF7AC74C".toLong(radix = 16))
+        Type.Water -> Color("FF6390F0".toLong(radix = 16))
+        Type.Fire -> Color("FFEE8130".toLong(radix = 16))
+        Type.Ground -> Color("FFE2BF65".toLong(radix = 16))
+        Type.Ghost -> Color("FF735797".toLong(radix = 16))
+        Type.Dragon -> Color("FF6F35FC".toLong(radix = 16))
+        Type.Ice -> Color("FF96D9D6".toLong(radix = 16))
+        Type.Rock -> Color("FFB6A136".toLong(radix = 16))
+        Type.Steel -> Color("FFB7B7CE".toLong(radix = 16))
+        Type.Fairy -> Color("FFD685AD".toLong(radix = 16))
+        Type.Electric -> Color("FFF7D02C".toLong(radix = 16))
+        Type.Dark -> Color("FF705746".toLong(radix = 16))
+        Type.Bug -> Color("FFA6B91A".toLong(radix = 16))
+        Type.Poison -> Color("FFA33EA1".toLong(radix = 16))
+        Type.Normal -> Color("FFA8A77A".toLong(radix = 16))
+        Type.Psychic -> Color("FFF95587".toLong(radix = 16))
+        Type.Fighting -> Color("FFC22E28".toLong(radix = 16))
+        Type.Flying -> Color("FFA98FF3".toLong(radix = 16))
+        Type.UNKNOWN -> Color("FFA8A77A".toLong(radix = 16))
         Type.NONE -> Color.Transparent
     }
 }
