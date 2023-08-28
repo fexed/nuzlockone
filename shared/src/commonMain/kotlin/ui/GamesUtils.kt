664ab7ef1e1356b8705ca67f87e7e4ca947838a1
Federico Matteoni <fedex15@live.it>
2023-08-28 20:57:39 +0200

    Cleanup

--- shared/src/commonMain/kotlin/ui/GamesUtils.kt
+++ shared/src/commonMain/kotlin/ui/GamesUtils.kt
@@ -26,17 +26,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cache
 import com.seiko.imageloader.rememberImagePainter
 import data.Game
-import data.Type
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import network.Cache
 import network.PokeApi
 
 @Composable
