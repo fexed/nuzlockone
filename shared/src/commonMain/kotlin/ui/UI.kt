bb71f77efd28bb6111dc969aebc2b651a1455e56
Federico Matteoni <fedex15@live.it>
2023-09-04 15:56:22 +0200

    Refactor

--- shared/src/commonMain/kotlin/ui/UI.kt
+++ shared/src/commonMain/kotlin/ui/UI.kt
@@ -1,11 +1,8 @@
 package ui
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExitTransition
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,12 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Button
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
@@ -32,16 +25,12 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.filled.Category
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.FilterListOff
 import androidx.compose.material.icons.filled.Map
-import androidx.compose.material.icons.filled.More
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.UnfoldLess
 import androidx.compose.material.icons.filled.VideogameAsset
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -55,6 +44,9 @@ import data.Type
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import widthClass
+
+lateinit var loaded: MutableState<Int>
+var toBeLoaded = 6
 
 @Composable
 fun TopBarFiltering(filterSelected: Boolean) {
@@ -249,8 +241,7 @@ fun BottomNavigationBar(currentSelected: Int, loading: Float = 0.5f, changeConte
 
 @Composable
 fun MainScaffold() {
-    var loaded by remember { mutableStateOf(0) }
-    val ITEMSTOLOAD = 6
+    loaded = remember { mutableStateOf(0) }
     FilterState.instance.currentSelectedType = remember { mutableStateOf(Type.NONE) }
     FilterState.instance.currentSelectedGame = remember { mutableStateOf(-1) }
     FilterState.instance.currentSelectedNuzlocke = remember { mutableStateOf(null) }
@@ -267,12 +258,12 @@ fun MainScaffold() {
         val scope = rememberCoroutineScope()
 
         scope.launch {
-            coroutineScope { cache.preloadTypes(); loaded++ }
-            coroutineScope { cache.preloadPokemons(); loaded++ }
-            coroutineScope { cache.preloadLocations(); loaded++ }
-            coroutineScope { cache.preloadGames(); loaded++ }
-            coroutineScope { cache.preloadItems(); loaded++ }
-            coroutineScope { cache.loadNuzlockes(); loaded++ }
+            coroutineScope { cache.preloadTypes(); loaded.value++ }
+            coroutineScope { cache.preloadPokemons(); loaded.value++ }
+            coroutineScope { cache.preloadLocations(); loaded.value++ }
+            coroutineScope { cache.preloadGames(); loaded.value++ }
+            coroutineScope { cache.preloadItems(); loaded.value++ }
+            coroutineScope { cache.loadNuzlockes(); loaded.value++ }
         }
 
         Scaffold(
@@ -287,7 +278,7 @@ fun MainScaffold() {
                 }
             },
             topBar = { TopBarFiltering(filterSelected) },
-            bottomBar = { BottomNavigationBar(content, loading = (loaded.toFloat()/ITEMSTOLOAD)) { content = it } }
+            bottomBar = { BottomNavigationBar(content, loading = (loaded.value.toFloat()/toBeLoaded)) { content = it } }
         )
     }
 }
