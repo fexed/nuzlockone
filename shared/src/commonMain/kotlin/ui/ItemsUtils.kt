c32acfdacc3eaa0f9ff49a0f74c43e7791b0a0cf
Federico Matteoni <fedex15@live.it>
2024-02-10 18:06:59 +0100

    Pokedollar icon

--- shared/src/commonMain/kotlin/ui/ItemsUtils.kt
+++ shared/src/commonMain/kotlin/ui/ItemsUtils.kt
@@ -2,10 +2,13 @@ package ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -21,13 +24,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.unit.dp
 import cache
 import com.seiko.imageloader.rememberImagePainter
 import data.Item
 import kotlinx.coroutines.coroutineScope
 import network.PokeApi
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.Resource
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun ItemCard(item: Item, isLoading: Boolean = false) {
     val painter = rememberImagePainter(item.imageURL)
@@ -47,7 +56,11 @@ fun ItemCard(item: Item, isLoading: Boolean = false) {
                     null,
                     modifier = Modifier.size(125.dp)
                 )
-                Text("${item.cost}")
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text("${item.cost}")
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Image(painter = painterResource("pokedollar.xml"), contentDescription = null, modifier = Modifier.size(15.dp), colorFilter = ColorFilter.tint(if (!isSystemInDarkTheme()) Color.Black else Color.White))
+                }
             } else {
                 Box(modifier = Modifier.size(100.dp))
             }
